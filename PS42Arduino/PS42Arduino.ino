714c13722d81e51334d29adc60915bd5cc10084c
kylecavs <kylecavorley@gmail.com>
2015-07-02 15:47:23 -0400

    Fix to code

--- PS42Arduino/PS42Arduino.ino
+++ PS42Arduino/PS42Arduino.ino
@@ -42,13 +42,10 @@ byte sensorPin       = 2;
 // The hall-effect flow sensor outputs approximately 7.5 pulses per second per
 // litre/minute of flow.
 float calibrationFactor = 7.5;
-
 volatile byte pulseCount;  
-
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
-
 unsigned long oldTime;
 
 void setup(void)
@@ -66,7 +63,6 @@ void setup(void)
  //flowMeter start 
   pinMode(statusLed, OUTPUT);
   digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
-  
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
 
@@ -159,77 +155,6 @@ void loop(void)
     // Enable the interrupt again now that we've finished sending output
     attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
   }
- 
- 
- //flowMeter Start
- 
-  if((millis() - oldTime) > 1000)    // Only process counters once per second
-  { 
-    // Disable the interrupt while calculating flow rate and sending the value to
-    // the host
-    detachInterrupt(sensorInterrupt);
-        
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    float time = millis() - oldTime; 
-    // Note the time this processing pass was executed. Note that because we've
-    // disabled interrupts the millis() function won't actually be incrementing right
-    // at this point, but it will still return the value it was set to just before
-    // interrupts went away.
-    oldTime = millis();
-    
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
-    // convert to millilitres.
-    flowMilliLitres = (flowRate / 60) * 1000;
-    
-    // Add the millilitres passed in this second to the cumulative total
-    totalMilliLitres += flowMilliLitres;
-      
-    unsigned int frac;
-    //Serial.println("*********************************************");
-    // Print the flow rate for this second in litres / minute
-   // Serial.print("Flow rate: ");
-   // Serial.print(int(flowRate));  // Print the integer part of the variable
-   // Serial.print(".");             // Print the decimal point
-    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
-    frac = (flowRate - int(flowRate)) * 10;
-   // Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-   // Serial.print("L/min");
-    // Print the number of litres flowed in this second
-    //Serial.print("  Current Liquid Flowing: ");             // Output separator
-    //Serial.print(flowMilliLitres);
-    //Serial.print("mL/Sec");
-
-    // Print the cumulative total of litres flowed since starting
-    //Serial.print("  Output Liquid Quantity: ");             // Output separator
-  //  Serial.print(totalMilliLitres);
-    //Serial.println("mL"); 
-    float inch3 = flowMilliLitres*0.0610237441; //1 mL = 0.06 cubic inches
-   // Serial.print("in^3/sec = ");
-    Serial.print(inch3);
-    float speed = inch3 / 0.15033011721; //in^3 / sec divided by area = pi*(3.5/16 in)^2
-    Serial.print(" ");
-    //Serial.print("Speed in/sec: ");
-    Serial.print(speed);
-   // Serial.println("in/sec");
-    
-   // Serial.print("time (s) = "); Serial.print(millis() - oldTime); Serial.print("  ");
-    //Serial.print(time);
-   // Serial.print("inches = ");
-   //Serial.println( (time)*speed);
-   // Serial.println("*********************************************");
-    // Reset the pulse counter so we can start incrementing again
-    pulseCount = 0;
-    
-    // Enable the interrupt again now that we've finished sending output
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
- 
 }
 
  double depth(double A, double B)
